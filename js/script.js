3392108d4311844c4cb752f53e1d842ee17fed8c
Eugene Pempel <pempel@cleverbits.co>
2016-09-07 22:48:31 +0300

    Учесть замечания наставника

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 var modal = document.querySelector(".modal");
+var overlay = document.querySelector(".overlay");
 var showLink = document.querySelector(".map-contacts a.button");
-var closeButton = modal.querySelector(".modal-content-close");
+var closeButton = modal.querySelector(".modal-close");
 
-if (modal && showLink && closeButton) {
+if (modal && overlay && showLink && closeButton) {
   showLink.addEventListener("click", function(event) {
     event.preventDefault();
     modal.classList.add("modal-show");
+    overlay.classList.add("overlay-show");
   });
 
   closeButton.addEventListener("click", function(event) {
     event.preventDefault();
     modal.classList.remove("modal-show");
+    overlay.classList.remove("overlay-show");
   });
 }
